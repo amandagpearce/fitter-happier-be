b8e39bda4a83a64e1cf4334854cf6c0273a547bf
amandagpearce <amandagpearce@gmail.com>
2023-04-02 10:41:13 -0300

    refactoring routes + better error handling

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM python:3.11
 # exposing the port where the app will run
 EXPOSE 5000
 COPY requirements.txt .
-# install flask
+# install requirements
 RUN pip install -r requirements.txt
 # copy everything (first .) to the directory we are at (second .)
 COPY . .
 # say what commands will run the container
-CMD ["flask", "run", "--host", "0.0.0.0", "--reload"]
+CMD ["flask", "run", "--host", "0.0.0.0"]
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask_cors import CORS
 from flask_openapi3 import OpenAPI, Info, Tag
 from flask import redirect, request
 import uuid
+from flask_smorest import abort
 
 from db import items, stores
 
@@ -26,7 +27,11 @@ def getDocs():
     return redirect("/openapi/swagger")
 
 
-# get all stores route
+#
+# STORES ROUTES
+#
+
+# get all stores
 @app.get("/stores", tags=[store_tag])
 def get_stores():
     """Faz a busca por todas as Stores cadastradas
@@ -36,10 +41,55 @@ def get_stores():
     if stores.values():
         return {"stores": list(stores.values())}
     else:
-        return {"message": "no stores found"}, 404
+        abort(404, message="No stores found")
 
 
-# get all items route
+# get store by id
+@app.route("/store/<string:store_id>", methods=["GET"])
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+# add new store
+@app.post("/store", tags=[store_tag])
+def create_store():
+    """Adiciona nova store"""
+
+    store_data = request.get_json()
+
+    if "name" not in store_data:
+        abort(
+            400, message="Bad request. Campo 'name' precisa existir no payload"
+        )
+
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message="Store já existe")
+
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
+
+    return new_store, 201
+
+
+@app.route("/store/<string:store_id>", methods=["DELETE"])
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+#
+# ITEMS ROUTES
+#
+
+# get all items
 @app.get("/items", tags=[store_tag])
 def get_items():
     """Faz a busca por todas os Items cadastrados
@@ -49,29 +99,37 @@ def get_items():
     if items.values():
         return {"items": list(items.values())}
     else:
-        return {"message": "no items found"}, 404
+        abort(404, message="No items found")
 
 
-# add new store route
-@app.post("/store", tags=[store_tag])
-def create_store():
-    """Adiciona nova store"""
-
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    new_store = {**store_data, "id": store_id}
-    stores[store_id] = new_store
-
-    return new_store, 201
-
-
-# add new item route
+# add new item
 @app.post("/item", tags=[store_tag])
 def create_item():
     item_data = request.get_json()
 
+    # checando se todos os campos estão presentes no payload
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="""Bad request. Inclua 'price', 'store_id' e 'name' no
+            payload""",
+        )
+
+    # checando se o item já não existe p/ prevenir items duplicados
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item já existe")
+
+    # checando se a store existe
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "id": item_id}
@@ -80,7 +138,7 @@ def create_item():
     return new_item, 201
 
 
-# get item by store id
+# get item by id
 # dynamic endpoint path issue
 # @app.get("/item/<string:item_id>") # doesn't work
 @app.route("/item/<string:item_id>", methods=["GET"])
@@ -88,13 +146,33 @@ def get_item(item_id):
     try:
         return items[item_id], 200
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
 
 
-# get store by id route
-@app.route("/store/<string:store_id>", methods=["GET"])
-def get_store(store_id):
+@app.route("/item/<string:item_id>", methods=["DELETE"])
+def delete_item(item_id):
     try:
-        return stores
+        del items[item_id]
+        return {"message": "Item deleted"}
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Item not found")
+
+
+@app.route("/item/<string:item_id>", methods=["PUT"])
+def update_item(item_id):
+    item_data = request.get_json()
+
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. 'price' e 'name' são necessário no payload",
+        )
+
+    try:
+        item = items[item_id]
+        item |= item_data  # |= in place modification to change the dictionary
+
+        return item
+
+    except KeyError:
+        abort(404, message="Item não encontrado")
